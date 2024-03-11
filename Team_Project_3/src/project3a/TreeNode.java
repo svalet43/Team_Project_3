c88c08f63b941db91b96ddc8896a86d63ce90a9f
svalet43 <159191290+svalet43@users.noreply.github.com>
2024-03-11 14:06:01 -0500

    Update TreeNode.java

--- Team_Project_3/src/project3a/TreeNode.java
+++ Team_Project_3/src/project3a/TreeNode.java
@@ -1,10 +1,10 @@
 package project3a;
 
 public class TreeNode {
-	public char val;
+	public String val;
 	public TreeNode left, right;
-	public TreeNode(char val) { this.val = val; }
-	public TreeNode(char val, TreeNode left, TreeNode right) {
+	public TreeNode(String val) { this.val = val; }
+	public TreeNode(String val, TreeNode left, TreeNode right) {
 		this.val = val;
 		this.left = left;
 		this.right = right;
